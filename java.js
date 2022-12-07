274c445667c9063c1edae9ad400c9a6b6ea85e36
SomiaMalallah <somiamalallah@gmail.com>
2022-12-07 20:08:44 +0300

    done

---
+++ java.js
@@ -0,0 +1,21 @@
+
+alert("مرحبا بك في موقعي !اسمي سمية وسوف احسب درجتك");
+
+let grade = prompt("درجتك")
+
+console.log (grade);
+
+
+if(grade >=90 ){
+    
+    console .log("لقد حصلت على امتياز ")
+
+}
+
+else if(grade >= 80){console .log("لقد حصلت على جيد جيدا")}
+else if(grade >=70){console .log("لقد حصلت على جيد")}
+else if(grade>=60){console .log("لقد حصلت على مقبول ")}
+else if (grade>=50){console .log("لقد حصلت على ضعيفٍ")}
+else{console.log("راسب")};
+
+
